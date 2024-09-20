f2eae41353e5a9b68cfd7c4cd680067cdae44e28
jakub.koza <jakub.koza@S50-12>
2024-09-20 09:24:06 +0200

    Dodaj pliki projektów.

---
+++ Album.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie17092024
+{
+    public class Album
+    {
+        public string artist;
+        public string album;
+        public int songsNumber;
+        public int year; 
+        public int downloadNumber;
+
+        public Album(string artist, string album, int songsNumber, int year, int downloadNumber)
+        {
+            this.artist = artist;
+            this.album = album;
+            this.songsNumber = songsNumber;
+            this.year = year;
+            this.downloadNumber = downloadNumber;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Zadanie17092024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Album> albumList = new List<Album>();
+
+            string filePath = "C:\\Users\\jakub.koza\\Downloads\\inf4_cz24_pliki2";
+            string[] fileLines = File.ReadAllLines(filePath);
+
+            int fileLinesEndIndex = fileLines.Length - 1;
+            
+            while (fileLinesEndIndex )
+            {
+
+            }
+
+
+
+
+        }
+    }
+}
